b0bb5dc8ddf1bbb452acd5e6899eb8df0072efc7
Acidiney Dias <acidiney.dias@digitalfactory.co.ao>
2019-12-18 18:28:27 +0100

    Add project files.

---
+++ WeSenderSDK/WeSender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WeSenderSDK
+{
+    public class WeSender
+    {
+        private string url = "http://apiwesender-dev.digitalfactory.co.ao";
+        private string apiKey;
+
+        public WeSender (string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public void sendMessage(MessageObject messageObject)
+        {
+            return;
+        }
+
+        protected class MessageObject
+        {
+            public Array destine;
+            public string message;
+            public bool hasSpecialCharacter = false;
+        }
+    }
+}
