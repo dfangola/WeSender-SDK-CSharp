cd0fd28359904a2f9e209d6f6e2cb4270705d6ed
Acidiney Dias <acidineydias@gmail.com>
2019-12-21 17:17:21 +0100

    Update Program.cs

--- TestSDK/Program.cs
+++ TestSDK/Program.cs
@@ -8,9 +8,9 @@ namespace TestSDK
     {
         static void Main(string[] args)
         {
-            var WSDK = new WeSender("589682376d834134b0be4b163a699ffb7d1d64d433544b35b9a53ce2b787413a");
+            var WSDK = new WeSender("apikey");
             var list = new List<string>();
-            list.Add("941056884");
+            list.Add("920000000");
             var payload = new MessageObjectModel();
             payload.destine = list;
             payload.message = "Olá";
